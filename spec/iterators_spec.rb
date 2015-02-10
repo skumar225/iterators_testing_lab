abf73b3a0c5e591c0ff5c23712844e23f7966683
Elie Schoppik <eschoppik@gmail.com>
2015-02-10 11:54:52 -0800

    adding more array methods

--- spec/iterators_spec.rb
+++ spec/iterators_spec.rb
@@ -4,6 +4,8 @@ describe "Iterators" do
 
 before :each do
   @arr = [1,2,3,4,5]
+  @hash = {}
+  @nil = nil
 end
   # select
   describe "#select" do
@@ -54,7 +56,7 @@ end
     end
   end
 
-  # one
+  # one?
   describe "#one" do
     it "should return true if only one element meets the condition" do
       expect([0,1].one? { |num| num == 0 }).to be(true)
@@ -64,13 +66,49 @@ end
     end
   end
 
-  # none
+  # none?
   describe "#none" do
     it "should return true if the number is not in the array " do
       expect(@arr.none? { |num| num == 6}).to be(true)
     end
     it "should return false if the number is in the array " do
       expect(@arr.none? { |num| num == 3}).to be(false)
+    end
+  end
+
+  #all?
+  describe "#all" do
+    it "should return true if the condition passes at all elements in the array" do
+      expect(@arr.all?{|num| num < 6}). to be(true)
+    end
+  end
+
+  #empty?
+  describe "#empty?" do
+    it "should return false if the array is not empty" do
+      expect(@arr.empty?). to be(false)
+    end
+  end
+
+  #eql?
+  describe "#eql?" do
+    it "should return true if an identical array is compared" do
+      expect(@arr.eql? [1,2,3,4,5]). to be(true)
+    end
+  end
+
+  #include?
+  describe "#include?" do
+    it "should return true if the parameter passed to include? is in the array" do
+      expect(@arr.include? 5). to be(true)
+      expect(@arr.include? 51). to be(false)
+    end
+  end
+
+  #nil?
+  describe "#nil?" do
+    it "should return true if the object is nil" do
+      expect(@nil.nil?). to be(true)
     end
   end
 end
