5044d73e4d8b94973c37989fa33c7353f089f1e1
prschulz <prschulz88@gmail.com>
2015-02-10 14:08:14 -0800

    removing answers from master

--- spec/iterators_spec.rb
+++ spec/iterators_spec.rb
@@ -18,98 +18,98 @@ end
   # reject
   describe "#reject" do
     it "should reject even numbers" do
-        expect(@arr.reject { |num|  num.even?  }).to eq([1,3,5])
+        expect().to eq()
       end
   end
 
   # collect / map
   describe "#map" do
     it "should square each number" do
-        expect(@arr.map { |num|  num*num  }).to eq([1,4,9,16,25])
+        expect().to eq()
       end
   end
 
   # detect / find
   describe "#detect" do
     it "should detect all numbers divisible by 2 and 4" do
-        expect(@arr.detect { |num| num % 2 == 0 and num % 4 == 0 }).to eq(4)
+        expect().to eq()
       end
   end
 
   # inject
   describe "#inject" do
     it "should sum up all the numbers" do
-      expect(@arr.inject { |num,prod| num+prod}).to eq(15)
+      expect().to eq()
     end
   end
 
   # partition
   describe "#partition" do
     it "should return an array of partitioned arrays " do
-      expect(@arr.partition { |num| num % 2 == 0}).to eq([[2,4],[1,3,5]])
+      expect().to eq()
     end
   end
 
   # sort
   describe "#sort" do
     it "should sort an array of numbers" do
-      expect([5,3,1,2,4].sort { |a,b| a-b }).to eq(@arr)
+      expect().to eq()
     end
   end
 
   # one?
   describe "#one" do
     it "should return true if only one element meets the condition" do
-      expect([0,1].one? { |num| num == 0 }).to be(true)
+      expect().to be()
     end
     it "should return false if more than one element meets the condition" do
-      expect([0,0,1].one? { |num| num == 0 }).to be(false)
+      expect().to be()
     end
   end
 
   # none?
   describe "#none" do
     it "should return true if the number is not in the array " do
-      expect(@arr.none? { |num| num == 6}).to be(true)
+      expect().to be()
     end
     it "should return false if the number is in the array " do
-      expect(@arr.none? { |num| num == 3}).to be(false)
+      expect().to be()
     end
   end
 
   #all?
   describe "#all" do
     it "should return true if the condition passes at all elements in the array" do
-      expect(@arr.all?{|num| num < 6}). to be(true)
+      expect().to be()
     end
   end
 
   #empty?
   describe "#empty?" do
     it "should return false if the array is not empty" do
-      expect(@arr.empty?). to be(false)
+      expect().to be()
     end
   end
 
   #eql?
   describe "#eql?" do
     it "should return true if an identical array is compared" do
-      expect(@arr.eql? [1,2,3,4,5]). to be(true)
+      expect().to be()
     end
   end
 
   #include?
   describe "#include?" do
     it "should return true if the parameter passed to include? is in the array" do
-      expect(@arr.include? 5). to be(true)
-      expect(@arr.include? 51). to be(false)
+      expect().to be(true)
+      expect().to be(false)
     end
   end
 
   #nil?
   describe "#nil?" do
     it "should return true if the object is nil" do
-      expect(@nil.nil?). to be(true)
+      expect().to be()
     end
   end
 end
